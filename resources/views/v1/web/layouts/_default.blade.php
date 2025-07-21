41381b83598221cd7d29b1c7ffbe9d66b2a98ff2
Yahya Hamdy <yahyahamdi650@gmail.com>
2025-07-21 16:16:39 +0100

    changes

--- resources/views/v1/web/layouts/_default.blade.php
+++ resources/views/v1/web/layouts/_default.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/x-icon" href="/v1/web/assets/images/favicon.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="/v1/web/assets/images/plivraison.png" />
     <link
         href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;display=swap"
         rel="stylesheet">
